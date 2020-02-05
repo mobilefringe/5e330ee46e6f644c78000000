6aa3d3155e0ce7c388fba21d3477f39af7767c6b
mithun@mobilefringe.com @ CodeCloud <mithun@mobilefringe.com @ CodeCloud>
2020-02-05 17:21:57 -0500

    Seacliff Village 2.0 1.0  /contact.vue

--- contact.vue
+++ contact.vue
@@ -21,7 +21,7 @@
                     </div>-->
                     <div class="row">
                         <div class="col-md-4">
-                            <div v-if="contactInfo" class="margin_60 padding_60" v-html="main.body"></div>    
+                            <div v-if="main.body.length" class="margin_60 padding_60" v-html="main.body"></div>    
                         </div>
                         <div class="col-md-8">
                             <transition name="fadeIn">
