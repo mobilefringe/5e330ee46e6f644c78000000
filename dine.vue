e39973e763bfa5236427773eb51ec91ab1966dc2
mithun@mobilefringe.com @ CodeCloud <mithun@mobilefringe.com @ CodeCloud>
2020-02-06 10:06:21 -0500

    Seacliff Village 2.0 1.0  /dine.vue

--- dine.vue
+++ dine.vue
@@ -199,9 +199,11 @@
                 dropDownCats() {
                     var vm = this;
                     var dining_cat =  _.find(this.processedCategories, function(o) { return o.name == "Dining"});
+                    dining_cat = dineFilter;
                     console.log("Dining Cat : " + dining_cat);
                     var subcategories = [];
                     if (dining_cat !== null && dining_cat !== undefined) {
+                        //dining_cat.id
                        subcategories = vm.findSubcategoriesByParentID(dining_cat.id);
                     }
                     
