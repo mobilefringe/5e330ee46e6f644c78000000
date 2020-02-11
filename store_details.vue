457751686cf535e2697b8aaabc0bb501e8f5604e
mithun@mobilefringe.com @ CodeCloud <mithun@mobilefringe.com @ CodeCloud>
2020-02-11 11:50:30 -0500

    Seacliff Village 2.0 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -48,7 +48,7 @@
                         </div>
                         <div class="col-md-8">
                             <div id="map" class="margin_20">
-                              <mapplic-png-map ref="pngmap_ref" :height="314" :hovertip="true" :storelist="allStores" :floorlist="floorList" :svgWidth="1500" :svgHeight="1500" @updateMap="updatePNGMap" id="store_details_map"></mapplic-png-map>  
+                              <mapplic-png-map ref="pngmap_ref" :height="314" :hovertip="true" :storelist="allStores" :floorlist="floorList" :svgWidth="property.map_image_width" :svgHeight="property.map_image_height" @updateMap="updatePNGMap" id="store_details_map"></mapplic-png-map>  
                             </div>
                             <div class=" margin_30 store_details_desc" v-html="currentStore.rich_description"></div>
                             <div v-if="this.currentStore.events">
@@ -315,7 +315,6 @@
                     this.svgMapRef.addActiveClass(this.currentStore.svgmap_region);
                 },
                 updatePNGMap(map) {
-                    console.log("MAP : " + map);
                     this.map = map;
                     this.dropPin(this.currentStore);
                 },
