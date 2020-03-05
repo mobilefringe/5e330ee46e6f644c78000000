6390eb242b8dbb86c8432952128ed8dd9ac623c8
mithun@mobilefringe.com @ CodeCloud <mithun@mobilefringe.com @ CodeCloud>
2020-03-05 14:52:07 -0500

    Seacliff Village 2.0 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -176,7 +176,7 @@
                     this.updateCurrentStore(this.$route.params.id);
                 },
                 currentStore: function () {
-                    this.currentStore.zoom = 2;
+                    this.currentStore.zoom = 1;
                     
                     if (_.includes(this.currentStore.store_front_url_abs, 'missing')) {
                         this.currentStore.no_logo = true
