5f9290ffe5c47298d1cd6aa5f06cc88f9c144071
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-04-15 15:14:56 -0400

    Seacliff Village 2.0 1.0  /main.js

--- main.js
+++ main.js
@@ -105,6 +105,10 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'vuex-router-syn
                                 href: "/dine"
                             },
                             {
+                                name: "Delivery",
+                                href: "/delivery"
+                            },
+                            {
                                 name: "Center Map",
                                 href: "/map"
                             }
