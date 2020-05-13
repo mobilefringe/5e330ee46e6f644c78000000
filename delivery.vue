8b10aac959260a13454da83031b0bbff9b65a751
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-05-13 10:44:51 -0400

    Seacliff Village 2.0 1.0  /delivery.vue

--- delivery.vue
+++ delivery.vue
@@ -188,7 +188,7 @@
             },
             created (){
                 this.loadData().then(response => {
-                    var temp_repo = this.findRepoByName('Dine Banner');
+                    var temp_repo = this.findRepoByName('Delivery Banner');
                     if (temp_repo !== null && temp_repo !== undefined) {
                        temp_repo = temp_repo.images;
                        this.pageBanner = temp_repo[0];
